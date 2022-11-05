02f1dcc336ce04f002161f3045ec3144c6b7b1a6
Ananya Garg <89492329+ananyakgarg@users.noreply.github.com>
2022-11-05 11:09:53 -0400

    Add files via upload

---
+++ teammate/teammateApp.swift
@@ -0,0 +1,17 @@
+//
+//  teammateApp.swift
+//  teammate
+//
+//  Created by Ananya Garg on 11/5/22.
+//
+
+import SwiftUI
+
+@main
+struct teammateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
